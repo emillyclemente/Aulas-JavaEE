3c47ddec953b597a4a4e6cdd5aff1c19e69bfded
emillyclemente <102821828+emillyclemente@users.noreply.github.com>
2022-06-09 14:16:37 -0300

    Update TabelaBean.java

--- TabelaDinamica/src/main/java/br/com/senai/laisyukari/application/bean/TabelaBean.java
+++ TabelaDinamica/src/main/java/br/com/senai/laisyukari/application/bean/TabelaBean.java
@@ -1,4 +1,4 @@
-package br.com.senai.laisyukari.application.bean;      //bean para interface
+package br.com.senai.laisyukari.application.bean;      // Ã‰ o Bean para interface
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,23 +16,23 @@ import br.com.senai.laisyukari.application.model.Despesa;
 
 public class TabelaBean implements Serializable{ 
 	
-	private List<Despesa> despesaList = new ArrayList<>();   //list para gravar tabela, para receber lista
-	private ListDataModel<Despesa> despesa = new ListDataModel<>(despesaList); //utilizado pelo jsf para construir a lista 
+	private List<Despesa> despesaList = new ArrayList<>();   //Ã‰ a lista para gravar tabela, para receber lista
+	private ListDataModel<Despesa> despesa = new ListDataModel<>(despesaList); // Ã‰ utilizado pelo JSF para construir a lista 
 	
-	public String inserir() {                             //inserção de crud em cima da lista, para novo objeto e adiciona ana lista
+	public String inserir() //InserÃ§Ã£o do CRUD em cima da lista, para um novo objeto e adiciona na lista
 		Despesa d = new Despesa();
 		d.setEdit(true);
 		despesaList.add(d);		
 		return null;
 	}
 	
-	public String excluir(Despesa despesa) {         //remoção da despesa da lista 
+	public String excluir(Despesa despesa) {   //Remove a despesa da lista 
 		despesaList.remove(despesa);
 		return null;
 	}
 	
-	public String editar(Despesa despesa) {   //unica funçao será manipular a informação
-		despesa.setEdit(true);                // true para renderizar novo botão
+	public String editar(Despesa despesa) {   //Unica funÃ§ao serÃ¡ manipular a informaÃ§Ã£o
+		despesa.setEdit(true);                // True para renderizar um novo botÃ£o
 		return null;
 	}
 	
@@ -41,8 +41,8 @@ public class TabelaBean implements Serializable{
 		return null;
 	}
 	
-	public ListDataModel<Despesa> getDespesa(){        //get captura informação para o jsf
-		return despesa;                                //retorno de despesa por conta do vinculo com o jsf, o qual utiliza para fazer a junção da tabela
+	public ListDataModel<Despesa> getDespesa(){        //O Get captura uma nova informaÃ§Ã£o para o jsf
+		return despesa;                                //Retorno de despesa por conta do vinculo com o JSF, o qual utiliza ela para fazer a junÃ§Ã£o da tabela
 	}
 }
 
